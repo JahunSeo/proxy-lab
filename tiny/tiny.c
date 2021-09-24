37931bbb6ad27c96aa4931e7d8a4cdd002094aa7
JahunSeo <wkgjs0809@gmail.com>
2021-09-24 16:41:16 +0900

    [johnny] tiny: clienterror /ing

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -32,7 +32,7 @@ int main(int argc, char **argv) {
   listenfd = Open_listenfd(argv[1]);
   while (1) {
     clientlen = sizeof(clientaddr);
-    printf("wait for client's requests... (%lu)\n", clientlen);
+    printf("wait for client's requests... (%d)\n", clientlen);
     connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);  // repeatedly accepting a connection request // line:netp:tiny:accept
     Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
     printf("Accepted connection from (%s, %s)\n", hostname, port);
@@ -44,11 +44,43 @@ int main(int argc, char **argv) {
 
 
 void doit(int fd) {
+  printf("doit doit chu~~\n");
   int is_static;  // client의 요청이 static content인지 여부
   struct stat sbuf;  // status of file을 저장하는 structure
   char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
   char filename[MAXLINE], cgiargs[MAXLINE];
   rio_t rio;
 
-  printf("doit!!\n");
+  /* Read request line and headers */
+  rio_readinitb(&rio, fd);  // paremeter connfd를 rio structure에 연결
+  rio_readlineb(&rio, buf, MAXLINE);  // connfd에 담겨 있는 client의 요청을 buf에 저장
+  printf("Request headers:\n");
+  printf("%s", buf);
+  sscanf(buf, "%s %s %s", method, uri, version);
+  // method가 GET이 아닌 경우 에러 처리
+  if (strcasecmp(method, "GET")) { // case-insensitive sting comparisons
+    clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+    return;
+  }
+
+}
+
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) {
+  char buf[MAXLINE], body[MAXBUF];
+
+  /* Build the HTTP response body */
+  sprintf(body, "<html><title>Tiny Error</title>");
+  sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);  
+  sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+
+  /* Print the HTTP response */
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-type: text/html\r\n");
+  rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+  rio_writen(fd, buf, strlen(buf));
+  rio_writen(fd, body, strlen(body));
 }
